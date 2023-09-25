bdcb2cc2835e9561a29ede1a9d8f593ee774a6de
Bryan Matheus <bryan.matheusbmf@gmail.com>
2023-09-25 13:49:24 -0300

    Merged all together

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { Elysia } from "elysia";
+import UserRepositoryMemory from "./external/memory/user-repository-memory";
+import RegisterUser from "./core/users/service/register-user";
+import RegisterUserController from "./adapters/register-user.controller";
+import UserRepositoryPrisma from "./external/prisma/user-repository-prisma";
+import FindAllUsers from "./core/users/service/find-all-users";
+import FindAllUsersController from "./adapters/fin-all-users.controller";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+const userRepository = new UserRepositoryPrisma();
+const registerUser = new RegisterUser(userRepository);
+const findAllUsers = new FindAllUsers(userRepository);
+
+new RegisterUserController(app, registerUser);
+new FindAllUsersController(app, findAllUsers);
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
